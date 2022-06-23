b8e22f212a80cbfbbc48c77fb5350d58d2010f3a
Adineu03 <106646056+Adineu03@users.noreply.github.com>
2022-06-23 14:24:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.listen(port);
 console.log(`Server running at http://localhost: ${port}`);
 
 app.get('/', (req, res) => {
+  const process_name = JSON.stringify(process);
+  res.send(process_name);
   const name = process.env.NAME || 'guest';
   res.send(`Hello ${name}!`);
 });
